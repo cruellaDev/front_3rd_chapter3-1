9c6a263b19fedec054e0ee6e1c5614a811c82b29
cruellaDev <cruellaDevLee@gmail.com>
2024-11-08 04:07:16 +0900

    * fix: dateUtils stripTime 수정

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -84,7 +84,7 @@ export function formatMonth(date: Date): string {
   return `${year}년 ${month}월`;
 }
 
-const stripTime = (d: Date) => new Date(d.getFullYear(), d.getMonth(), d.getDate());
+const stripTime = (d: Date) => new Date(d.getFullYear(), d.getMonth(), d.getDate()).getTime();
 /**
  * 주어진 날짜가 특정 범위 내에 있는지 확인합니다.
  */
